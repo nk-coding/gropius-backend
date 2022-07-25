ee7e535d066273290b926ff517b602bdf2fa7d98
nk-coding <niklask.coding@gmail.com>
2022-07-25 19:50:42 +0200

    project mutations

--- api/src/main/kotlin/gropius/schema/mutation/ArchitectureMutations.kt
+++ api/src/main/kotlin/gropius/schema/mutation/ArchitectureMutations.kt
@@ -6,19 +6,27 @@ import com.expediagroup.graphql.server.operations.Mutation
 import graphql.schema.DataFetchingEnvironment
 import gropius.authorization.gropiusAuthorizationContext
 import gropius.dto.input.architecture.CreateComponentInput
+import gropius.dto.input.architecture.CreateProjectInput
 import gropius.dto.input.architecture.UpdateComponentInput
+import gropius.dto.input.architecture.UpdateProjectInput
 import gropius.dto.input.common.DeleteNodeInput
 import gropius.graphql.AutoPayloadType
 import gropius.model.architecture.Component
+import gropius.model.architecture.Project
 import gropius.service.architecture.ComponentService
+import gropius.service.architecture.ProjectService
 
 /**
  * Contains all architecture-related mutations
  *
- * @param componentService used for component-related mutations
+ * @param componentService used for Component-related mutations
+ * @param projectService used for Project-related mutations
  */
 @org.springframework.stereotype.Component
-class ArchitectureMutations(private val componentService: ComponentService) : Mutation {
+class ArchitectureMutations(
+    private val componentService: ComponentService,
+    private val projectService: ProjectService
+) : Mutation {
 
     @GraphQLDescription(
         """Creates a new Component, requires CAN_CREATE_COMPONENTS.
@@ -52,6 +60,41 @@ class ArchitectureMutations(private val componentService: ComponentService) : Mu
         dfe: DataFetchingEnvironment
     ): ID {
         componentService.deleteComponent(dfe.gropiusAuthorizationContext, input)
+        return input.id
+    }
+
+    @GraphQLDescription(
+        """Creates a new Project, requires CAN_CREATE_PROJECTS.
+        Automatically generates a default ProjectPermission which grants the user READ and ADMIN
+        """
+    )
+    @AutoPayloadType("The created Project")
+    suspend fun createProject(
+        @GraphQLDescription("Defines the created Project")
+        input: CreateProjectInput,
+        dfe: DataFetchingEnvironment
+    ): Project {
+        return projectService.createProject(dfe.gropiusAuthorizationContext, input)
+    }
+
+    @GraphQLDescription("Updates the specified Project, requires ADMIN on the project to update")
+    @AutoPayloadType("The updated Project")
+    suspend fun updateProject(
+        @GraphQLDescription("Defines which Project to update and how to update it")
+        input: UpdateProjectInput,
+        dfe: DataFetchingEnvironment
+    ): Project {
+        return projectService.updateProject(dfe.gropiusAuthorizationContext, input)
+    }
+
+    @GraphQLDescription("Deletes the specified Project, requires ADMIN on the project to delete")
+    @AutoPayloadType("The id of the deleted Project")
+    suspend fun deleteProject(
+        @GraphQLDescription("Defines which Project to delete")
+        input: DeleteNodeInput,
+        dfe: DataFetchingEnvironment
+    ): ID {
+        projectService.deleteProject(dfe.gropiusAuthorizationContext, input)
         return input.id
     }
 
--- core/src/main/kotlin/gropius/service/architecture/ProjectService.kt
+++ core/src/main/kotlin/gropius/service/architecture/ProjectService.kt
@@ -1,13 +1,88 @@
 package gropius.service.architecture
 
+import gropius.authorization.GropiusAuthorizationContext
+import gropius.dto.input.architecture.CreateProjectInput
+import gropius.dto.input.architecture.UpdateProjectInput
+import gropius.dto.input.common.DeleteNodeInput
 import gropius.model.architecture.Project
+import gropius.model.user.permission.GlobalPermission
+import gropius.model.user.permission.NodePermission
 import gropius.repository.architecture.ProjectRepository
+import gropius.repository.findById
+import gropius.service.user.permission.ProjectPermissionService
+import io.github.graphglue.authorization.Permission
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Service
 
 /**
  * Service for [Project]s. Provides functions to create, update and delete
  *
  * @param repository the associated repository used for CRUD functionality
+ * @param projectPermissionService used to create the initial permission for a created [Project]
  */
 @Service
-class ProjectService(repository: ProjectRepository) : TrackableService<Project, ProjectRepository>(repository)
+class ProjectService(repository: ProjectRepository, val projectPermissionService: ProjectPermissionService) :
+    TrackableService<Project, ProjectRepository>(repository) {
+
+    /**
+     * Creates a new [Project] based on the provided [input]
+     * Checks the authorization status
+     *
+     * @param authorizationContext used to check for the required permission
+     * @param input defines the [Project]
+     * @return the saved created [Project]
+     */
+    suspend fun createProject(
+        authorizationContext: GropiusAuthorizationContext, input: CreateProjectInput
+    ): Project {
+        input.validate()
+        val user = getUser(authorizationContext)
+        checkPermission(
+            user, Permission(GlobalPermission.CAN_CREATE_PROJECTS, authorizationContext), "create Projects"
+        )
+        val project = Project(input.name, input.description, input.repositoryURL)
+        createdExtensibleNode(project, input)
+        projectPermissionService.createDefaultPermission(user, project)
+        return repository.save(project).awaitSingle()
+    }
+
+    /**
+     * Updates a [Project] based on the provided [input]
+     * Checks the authorization status
+     *
+     * @param authorizationContext used to check for the required permission
+     * @param input defines which [Project] to update and how
+     * @return the updated [Project]
+     */
+    suspend fun updateProject(
+        authorizationContext: GropiusAuthorizationContext, input: UpdateProjectInput
+    ): Project {
+        input.validate()
+        val project = repository.findById(input.id)
+        checkPermission(
+            project, Permission(NodePermission.ADMIN, authorizationContext), "update the Project"
+        )
+        updateTrackable(project, input)
+        return repository.save(project).awaitSingle()
+    }
+
+    /**
+     * Deletes a [Project] by id
+     * Checks the authorization status
+     *
+     * @param authorizationContext used to check for the required permission
+     * @param input defines which [Project] to delete
+     */
+    suspend fun deleteProject(
+        authorizationContext: GropiusAuthorizationContext, input: DeleteNodeInput
+    ) {
+        input.validate()
+        val project = repository.findById(input.id)
+        checkPermission(
+            project, Permission(NodePermission.ADMIN, authorizationContext), "delete the Project"
+        )
+        beforeDeleteTrackable(project)
+        repository.delete(project).awaitSingle()
+    }
+
+}

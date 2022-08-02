df449e332decc607b9469853a001bcdd222ca88d
nk-coding <niklask.coding@gmail.com>
2022-08-02 19:01:11 +0200

    improve AutoPayloadType documentation

--- api/src/main/kotlin/gropius/graphql/AutoPayloadType.kt
+++ api/src/main/kotlin/gropius/graphql/AutoPayloadType.kt
@@ -3,6 +3,10 @@ package gropius.graphql
 /**
  * Can be applied to GraphQL mutation functions
  * Results in automatically generated payload return types
+ * The generated type has a single field, which is the return value of the method
+ * annotated with this annotation
+ *
+ * @param description the description of the single field of the generated payload type
  */
 @MustBeDocumented
 @Target(AnnotationTarget.FUNCTION)

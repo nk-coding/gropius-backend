f30a8514c59d8e26c414c58987931af152dad7c9
nk-coding <niklask.coding@gmail.com>
2022-08-14 08:19:17 +0200

    add graphql documentation

---
+++ website/scripts/generate-graphql-schema.sh
@@ -0,0 +1,18 @@
+mkdir -p ./website/schemas
+./gradlew api:bootRun &
+gradlew_pid=$!
+schema_endpoint="http://localhost:8080/sdl"
+c=0
+until curl -s -f -o /dev/null $schema_endpoint
+do
+    ((c++))
+    if ((c > 120)); then
+        echo "Failed to get graphql schema: timeout"
+        exit 1
+    fi
+    echo "Waiting for server"
+    sleep 2
+done
+curl -s -o ./website/schemas/api-public.gql $schema_endpoint
+echo "Stopping graphql server"
+kill $gradlew_pid
---
+++ website/scripts/update-graphql-doc.sh
@@ -0,0 +1,3 @@
+cd website
+npx docusaurus graphql-to-doc:api-public -f
+rm docs/graphql/api-public/generated.md

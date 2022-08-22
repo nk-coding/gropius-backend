7dd0664a5713600fb3d0e18737d43435504d17b8
nk-coding <niklask.coding@gmail.com>
2022-08-22 16:56:30 +0200

    add missing documentation

--- core/src/main/kotlin/gropius/repository/GropiusRepository.kt
+++ core/src/main/kotlin/gropius/repository/GropiusRepository.kt
@@ -4,5 +4,11 @@ import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository
 import org.springframework.data.neo4j.repository.support.ReactiveCypherdslConditionExecutor
 import org.springframework.data.repository.NoRepositoryBean
 
+/**
+ * Meta-repository that included [ReactiveNeo4jRepository] and [ReactiveCypherdslConditionExecutor]
+ *
+ * @param T the type of node
+ * @param ID the type of id used
+ */
 @NoRepositoryBean
 interface GropiusRepository<T, ID> : ReactiveNeo4jRepository<T, ID>, ReactiveCypherdslConditionExecutor<T>

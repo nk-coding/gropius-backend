594aa1d062633212494306df27f3b24088743f6c
Christian Kurz <chrikuvellberg@gmail.com>
2022-08-23 22:04:18 +0200

    explanation of json string for

--- github/src/main/kotlin/gropius/sync/github/JsonHelper.kt
+++ github/src/main/kotlin/gropius/sync/github/JsonHelper.kt
@@ -13,6 +13,7 @@ class JsonHelper(
 ) {
     /**
      * Parse a json value into a string
+     * For example a `"text"` becomes `text`, while `"text` results in null
      * @param input the json value stringified
      * @return null if null or not a valid json string
      */

95e766e888b622bce1397c183141025ead868dcb
Christian Kurz <chrikuvellberg@gmail.com>
2022-09-18 20:50:20 +0200

    emptyList

--- github/src/main/kotlin/gropius/sync/github/Outgoing.kt
+++ github/src/main/kotlin/gropius/sync/github/Outgoing.kt
@@ -212,7 +212,7 @@ class Outgoing(
                     }
                 }
             } else {
-                listOf();
+                emptyList()
             }
         } else {
             return listOf {
